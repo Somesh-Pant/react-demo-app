4f1227cb1c685917992ab7be06ba1fee1438e676
Somesh <someshpant7@gmail.com>
2022-02-26 18:26:12 +0530

    basic single page app(SPA) in react.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text = "text 1"/>
+      <Todo text = "text 2"/>
+      <Todo text = "text 3"/>
+    </div> 
+  );
+}
+
+export default App;
